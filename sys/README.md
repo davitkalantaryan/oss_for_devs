ca8c4989362f81579fe1b15fdc4c1860662d3a2c
Davit Kalantaryan <davit.kalantar@desy.de>
2019-12-30 13:55:39 +0100

    Initial commit to repository oss_for_devs

---
+++ sys/README.md
@@ -0,0 +1,8 @@
+# sys for oss_for_devs  
+
+Subfolders of this folder named using 'lsb_release -c | cut -f 2' command for LINUX boxs.  
+The subfolders are following  
+  1.  common  (common stuff)
+  2.  bionic (ubuntu18)  
+  3.  Carbon (SL6)  
+  4.  Santiago (SL6)  
---
+++ sys/bionic/post_install_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This script prepares freshly installed MTCA host
+# 
+# Author:  Davit Kalantaryan (Email: davit.kalantaryan@desy.de)
+# Detailed documentation: https://www-zeuthen.desy.de/~kalantar/public/prepare/prepare.html
+# If there is a link to above mentioned document, then it will be like this (p. x), where x 
+# is the part pf the document 
+#
+# Usefull links
+# https://serverfault.com/questions/410338/adding-local-users-passwords-on-kerberized-linux-box
+#
